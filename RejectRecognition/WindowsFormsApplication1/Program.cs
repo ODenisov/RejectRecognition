877c37e6ee6808225c7e0b1c8b36b9f83a254766
Oleg Denisov <Jamesbdigriz@yandex.ru>
2017-01-23 17:03:47 +0300

    starting

---
+++ RejectRecognition/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            IplImage lol = new IplImage();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
